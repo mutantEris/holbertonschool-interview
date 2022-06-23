b434b8ef1472af7283a9adc96ea1de1c5f7e5303
mutantEris <mmires@live.com>
2022-06-23 15:53:10 -0500

    minimumimimimum

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Minimum number of operations"""
+def minOperations(n):
+    """Smallest Number Of Operations"""
+    pumpkin = 0
+    operations = 0
+    total = 1
+
+    if n < 2:
+        return 0
+    while total < n:
+        if n % total == 0:
+            pumpkin = total
+            operations += 1
+        total += pumpkin
+        operations += 1
+    return operations
