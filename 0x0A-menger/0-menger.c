4f378274e21d0f5b491ad748523e7f2c5254d7e7
mutantEris <mmires@live.com>
2022-09-12 12:23:49 -0500

    styling

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -1,40 +1,42 @@
 #include "menger.h"
 
+/**
+ * menger - draws a Menger Sponge but with 2 dimensions
+ * @level: menger sponge bigness level
+ */
 
 void menger(int level)
 {
-    int x;
-    int y;
-    int g;
-    int h;
-    int z;
-    int length = pow(3, level);
+	int x;
+	int y;
+	int g;
+	int h;
+	int z;
+	int length = pow(3, level);
 
-    if(level <= 0)
-    {
-        printf("#\n");
-    }
-    else
-    {
-        for(y = 0; y < length; y++)
-        {
-            for(x = 0; x < length; x++)
-            {
-                g = x;
-                h = y;
-                for (z = 0; z < level; z++)
-                {
-                    if (g % 3 == 1 && h % 3 == 1)
-                        break;
-                    g /= 3;
-                    h /= 3;
-                }
-                if (z == level)
-                    printf("#");
-                else 
-                    printf(" ");
-            }
-            printf("\n");
-        }
-    }
+	if(level <= 0)
+	    return;
+	else
+	{
+		for(y = 0; y < length; y++)
+		{
+			for(x = 0; x < length; x++)
+			{
+				g = x;
+				h = y;
+				for (z = 0; z < level; z++)
+				{
+					if (g % 3 == 1 && h % 3 == 1)
+						break;
+					g /= 3;
+					h /= 3;
+				}
+				if (z == level)
+					printf("#");
+				else 
+					printf(" ");
+			}
+			printf("\n");
+		}
+	}
 }
