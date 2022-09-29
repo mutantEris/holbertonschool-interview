bd9e61e7d621a99736d91c0df609a7f6f7cd7aa4
mutantEris <mmires@live.com>
2022-09-29 16:53:15 -0500

    spaces

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -4,6 +4,8 @@ Given a list of non-negative integers representing the heights of walls with
 unit width 1, as if viewing the cross-section of a relief map, calculate how
 many square units of water will be retained after it rains.
 """
+
+
 def rain(walls):
     """
     walls: list of non-negative integers.
