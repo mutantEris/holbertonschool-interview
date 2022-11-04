9ea3c436f7b3e693afb6884c8eeaf1ed94aaf4aa
mutantEris <mmires@live.com>
2022-11-03 19:24:40 -0500

    pep 8 stinky

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,7 +4,7 @@
 
 def rotate_2d_matrix(matrix):
     """rotates matrix clockwise 90 degrees"""
-    size= len(matrix[0])
+    size = len(matrix[0])
     for x in range(size // 2):
         for g in range(x, size - x - 1):
             temp = matrix[x][g]
@@ -12,4 +12,3 @@ def rotate_2d_matrix(matrix):
             matrix[size - 1 - g][x] = matrix[size - 1 - x][size - 1 - g]
             matrix[size - 1 - x][size - 1 - g] = matrix[g][size - 1 - x]
             matrix[g][size - 1 - x] = temp
-            
