6a9c9a7f31cc44375faa3cd336e500f635d574fb
mutantEris <mmires@live.com>
2022-11-17 20:08:27 -0600

    pep8

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -4,10 +4,11 @@
 
 def makeChange(coins, total):
     '''Counts the coins I found in my couch'''
+    if total <= 0:
+        return 0
     count = 0
     coins.sort(reverse=True)
     for coin in coins:
         count += total // coin
         total %= coin
     return -1 if total else count
-    
